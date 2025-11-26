3fe75cd24d683cd05b84284956ab6250543bdd35
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-11-26 11:16:33 +0200

    SignUp&SignIn DONE

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div>
-      <p>Admin dashboard</p>
-      <Button>Hellonpm install @clerk/nextjs</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
-}
+};
+export default SetupPage;
