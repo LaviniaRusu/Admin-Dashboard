4d5883d1799a195befbdc2bb0dc36543c054c1d1
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-12-09 14:04:18 +0200

    Products Entity-Products page -> part 1

---
+++ src/app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -0,0 +1,50 @@
+import prisma from "@/lib/prisma";
+import { BillboardForm, ProductForm } from "./components/product-form";
+import ImageUpload from "@/components/ui/image-upload";
+
+const ProductPage = async ({
+  params,
+}: {
+  params: Promise<{ productId: string; storeId: string }>;
+}) => {
+  const { productId, storeId } = await params;
+  const product = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+    include: {
+      images: true,
+    },
+  });
+
+  const categories = await prisma.category.findMany({
+    where: {
+      storeId: storeId,
+    },
+  });
+
+  const sizes = await prisma.size.findMany({
+    where: {
+      storeId: storeId,
+    },
+  });
+
+  const colors = await prisma.color.findMany({
+    where: {
+      storeId: storeId,
+    },
+  });
+
+  return (
+    <div>
+      <ProductForm
+        categories={categories}
+        colors={colors}
+        sizes={sizes}
+        initialData={product}
+      />
+    </div>
+  );
+};
+
+export default ProductPage;
