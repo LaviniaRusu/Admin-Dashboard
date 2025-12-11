593eb2b69b51a23dcd5502f2744a94faae73626c
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-12-11 21:07:26 +0200

    Environment setup & featured products

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,17 @@
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware();
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-//     // Always run for API routes
-//     "/(api|trpc)(.*)",
-//   ],
-// };
-// REMOVE this entire file if you don't need auth for API routes.
-// Or use the new proxy system:
-// import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// export default clerkMiddleware({
-//   publicRoutes: ["/api/:path*"],
-// });
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/api/stores"],
-// };
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/api/:path*"]);
+//  DOAR rutele de admin sunt protejate
+const isProtectedRoute = createRouteMatcher([
+  "/api/:storeId/(billboards|categories|products|sizes|colors)", // POST etc
+  "/(.*)/settings",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+  if (isProtectedRoute(req) && req.method !== "GET") {
+    await auth.protect();
+  }
 });
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/api/stores", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api|trpc)(.*)"],
 };
