4be2aab1edd1bead7b6d9c71053249dc0d9e0af6
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2025-12-14 15:39:13 +0200

    Dashboard page (Admin) -> with bug

--- src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,6 +1,42 @@
-import React from "react";
-import prisma from "@/lib/prisma";
+// import React from "react";
+// import prisma from "@/lib/prisma";
 
+// const DashboardPage = async ({
+//   params,
+// }: {
+//   params: Promise<{ storeId: string }>;
+// }) => {
+// const { storeId } = await params;
+//   const store = await prisma.store.findFirst({
+//     where: {
+//       id: storeId,
+//     },
+//   });
+
+//   return <div>Active Store: {store?.name}</div>;
+// };
+
+// export default DashboardPage;
+import React from "react";
+import { CreditCard, DollarSign, Package } from "lucide-react";
+
+import prisma from "@/lib/prisma";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { formatter } from "@/lib/utils";
+import { getTotalRevenue } from "../../../../../actions/get-total-revenue";
+import { getSalesCount } from "../../../../../actions/get-sales-count";
+import { getStockCount } from "../../../../../actions/get-stock-count";
+import { getGraphRevenue } from "../../../../../actions/get-graph-revenue";
+
+import { Overview } from "@/components/overview";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+}
 const DashboardPage = async ({
   params,
 }: {
@@ -12,8 +48,68 @@ const DashboardPage = async ({
       id: storeId,
     },
   });
+  const totalRevenue = await getTotalRevenue(storeId);
+  const salesCount = await getSalesCount(storeId);
+  const stockCount = await getStockCount(storeId);
+  const graphRevenue = await getGraphRevenue(storeId);
 
-  return <div>Active Store: {store?.name}</div>;
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+
+        <div className="grid gap-4 grid-cols-3">
+          {/* Total Revenue */}
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+
+          {/* Sales */}
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+{salesCount}</div>
+            </CardContent>
+          </Card>
+
+          {/* Products in Stock */}
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products In Stock
+              </CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{stockCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/app/api/[storeId]/checkout/route.ts
+++ src/app/api/[storeId]/checkout/route.ts
@@ -1,87 +1,146 @@
+// import Stripe from "stripe";
+// import { headers } from "next/headers";
+// import { NextResponse } from "next/server";
+
+// import { stripe } from "@/lib/stripe";
+// import prisma from "@/lib/prisma";
+
+// export async function POST(req: Request) {
+//   const body = await req.text();
+//   const signature = (await headers().get("Stripe-Signature")) as string;
+
+//   let event: Stripe.Event;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       body,
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET!
+//     );
+//   } catch (error: any) {
+//     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+//   }
+
+//   const session = event.data.object as Stripe.Checkout.Session;
+//   const address = session.customer_details?.address;
+
+//   const addressComponents = [
+//     address?.line1,
+//     address?.line2,
+//     address?.city,
+//     address?.state,
+//     address?.postal_code,
+//     address?.country,
+//   ];
+
+//   const addressString = addressComponents.filter((c) => c !== null).join(", ");
+
+//   if (event.type === "checkout.session.completed") {
+//     const order = await prisma.order.update({
+//       where: {
+//         id: session.metadata?.orderId,
+//       },
+//       data: {
+//         isPaid: true,
+//         address: addressString,
+//         phone: session.customer_details?.phone || "",
+//       },
+//       include: {
+//         orderItems: true,
+//       },
+//     });
+
+//     const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+//     await prisma.product.updateMany({
+//       where: {
+//         id: {
+//           in: [...productIds],
+//         },
+//       },
+//       data: {
+//         isArchived: true,
+//       },
+//     });
+//   }
+
+//   return new NextResponse(null, { status: 200 });
+// }
 import Stripe from "stripe";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe";
 import prisma from "@/lib/prisma";
 
-const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization",
-};
+export async function POST(req: Request) {
+  const body = await req.text();
 
-export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
-}
+  const headersList = await headers();
+  const signature = headersList.get("Stripe-Signature") as string;
 
-export async function POST(
-  req: Request,
-  { params }: { params: Promise<{ storeId: string }> }
-) {
-  const { storeId } = await params;
-  const { productIds } = await req.json();
+  let event: Stripe.Event;
 
-  if (!productIds || productIds.length === 0) {
-    return new NextResponse("Product ids are required", { status: 400 });
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return new NextResponse(`Webhook Error: ${error.message}`, {
+        status: 400,
+      });
+    }
+
+    return new NextResponse("Webhook Error", { status: 400 });
   }
 
-  const products = await prisma.product.findMany({
-    where: {
-      id: {
-        in: productIds,
+  const session = event.data.object as Stripe.Checkout.Session;
+  const address = session.customer_details?.address;
+
+  const addressComponents = [
+    address?.line1,
+    address?.line2,
+    address?.city,
+    address?.state,
+    address?.postal_code,
+    address?.country,
+  ];
+
+  const addressString = addressComponents
+    .filter((c): c is string => Boolean(c))
+    .join(", ");
+
+  if (event.type === "checkout.session.completed") {
+    const order = await prisma.order.update({
+      where: {
+        id: session.metadata?.orderId,
       },
-    },
-  });
-
-  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  products.forEach((product) => {
-    line_items.push({
-      quantity: 1,
-      price_data: {
-        currency: "USD",
-        product_data: {
-          name: product.name,
-        },
-        unit_amount: product.price.toNumber() * 100,
+      data: {
+        isPaid: true,
+        address: addressString,
+        phone: session.customer_details?.phone || "",
+      },
+      include: {
+        orderItems: true,
       },
     });
-  });
 
-  const order = await prisma.order.create({
-    data: {
-      storeId: storeId,
-      isPaid: false,
-      orderItems: {
-        create: productIds.map((productId: string) => ({
-          product: {
-            connect: {
-              id: productId,
-            },
-          },
-        })),
+    const productIds = order.orderItems.map((orderItem) => orderItem.productId);
+
+    await prisma.product.updateMany({
+      where: {
+        id: {
+          in: productIds,
+        },
       },
-    },
-  });
+      data: {
+        isArchived: true,
+      },
+    });
+  }
 
-  const session = await stripe.checkout.sessions.create({
-    line_items,
-    mode: "payment",
-    billing_address_collection: "required",
-    phone_number_collection: {
-      enabled: true,
-    },
-    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata: {
-      orderId: order.id,
-    },
-  });
-
-  return NextResponse.json(
-    { url: session.url },
-    {
-      headers: corsHeaders,
-    }
-  );
+  return new NextResponse(null, { status: 200 });
 }
