9be103ea4c64ec807399e4b9da42871f94fa658d
Rusu Lavinia- Teodora <laviniateodora3@gmail.com>
2026-04-03 21:22:26 +0300

    any in page.tsx bill

--- src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -17,11 +17,13 @@ const BillboardsPage = async ({
       createdAt: "desc",
     },
   });
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-    id: item.id,
-    label: item.label,
-    createdAt: format(item.createdAt, "MMMM do, yyyy"),
-  }));
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (item: any) => ({
+      id: item.id,
+      label: item.label,
+      createdAt: format(item.createdAt, "MMMM do, yyyy"),
+    }),
+  );
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
